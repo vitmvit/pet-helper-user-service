1914bd43cdf5b87e2eb0547a6b9f39d538ec9040
vitmvit <120332119+vitmvit@users.noreply.github.com>
2025-12-21 23:22:05 +0300

    Delete exceptions. Refactored build.gradle.

--- src/main/java/by/vitikova/discovery/exception/InvalidJwtException.java
+++
@@ -1,8 +0,0 @@
-package by.vitikova.discovery.exception;
-
-public class InvalidJwtException extends RuntimeException {
-
-    public InvalidJwtException(String message) {
-        super(message);
-    }
-}
